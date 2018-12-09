723ba8fb10d05dc0646c1bca6f1002fb3b37af98
DagonLi <527493644@qq.com>
2018-12-09 14:20:56 +0800

    modify br_dagon

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello DagonLi,hold on,you will get what you want!");
     }
 }
