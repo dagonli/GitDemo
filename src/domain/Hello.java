eadbe28e259371d996683be9a813b1755a37a4de
DagonLi <527493644@qq.com>
2018-12-09 14:15:20 +0800

    modify hello

--- src/domain/Hello.java
+++ src/domain/Hello.java
@@ -4,4 +4,7 @@ package domain;
  * Created by Dagon on 2018/12/9 - 12:25
  */
 public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello,ping an tech");
+    }
 }
