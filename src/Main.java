9a538ea404cc8b8fe686cac80818baa18d78dec4
DagonLi <527493644@qq.com>
2018-12-09 17:44:38 +0800

    Merge branch 'master' of https://github.com/dagonli/GitDemo into dagon
    # Conflicts:
    #	src/Main.java
    keep it .

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is main method!");
+
         System.out.println("I am SofiaLea.");
+
+        System.out.println("I am DagonLea.");
+
     }
 }
