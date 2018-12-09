bcecab1b68b8d0edcc3d3555dcb7f77db16ab89f
DagonLi <527493644@qq.com>
2018-12-09 14:28:25 +0800

    main commit.

--- src/domain/User.java
+++ src/domain/User.java
@@ -8,6 +8,16 @@ public class User {
     private String name;
     private int age;
 
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
